4ee9afdd19ae1771dea7ca6f6bd0b1269205667d
Aparna Sharma <aparna07@ccs.neu.edu>
2018-02-03 01:28:25 -0500

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+## Integrating Spring Boot with MySQL
+
+[Insert a static hello message](http://cs5200-spring2018-sharma.us-east-1.elasticbeanstalk.com/api/hello/insert)
+
+[Insert a parameterized hello message](http://cs5200-spring2018-sharma.us-east-1.elasticbeanstalk.com/api/hello/insert/Some%20parameterized%20message)
+
+[Retrieve all hello messages](http://cs5200-spring2018-sharma.us-east-1.elasticbeanstalk.com/api/hello/select/all)
--- src/main/java/edu/northeastern/cs5200/Cs5200Spring2018SharmaApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018SharmaApplication.java
@@ -2,9 +2,21 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Spring2018SharmaApplication {
+public class Cs5200Spring2018SharmaApplication extends SpringBootServletInitializer {
+
+	@Override
+
+	protected SpringApplicationBuilder
+
+			configure(SpringApplicationBuilder application) {
+
+		return application.sources(Cs5200Spring2018SharmaApplication.class);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Spring2018SharmaApplication.class, args);
